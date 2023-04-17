b1253729f208a1721ca69bf1a2be51cb846258b0
Vasilij U <vasya-1001@yandex.ru>
2023-04-17 21:55:46 +0500

    Zadacha 15 completed

--- Zadacha_15/Program.cs
+++ Zadacha_15/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int d;
+
+Console.Write("Введите число дня недели, от 1 до 7: ");
+if(int.TryParse(Console.ReadLine(), out d))
+{
+    d=d-1;
+    if (d<7)
+    {
+        System.Console.Write("День недели является выходным? - ");
+        if (d==0) Console.WriteLine("Нет");
+        else if (d==1) Console.WriteLine("Нет");
+        else if (d==2) Console.WriteLine("Нет");
+        else if (d==3) Console.WriteLine("Нет");
+        else if (d==4) Console.WriteLine("Нет");
+        else if (d==5) Console.WriteLine("Да");
+        else if (d==6) Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you're wrong. Day do not exist!");
+    }
+}
+else
+{
+    Console.WriteLine("Sorry, you're wrong. Day do not exist!");
+}
