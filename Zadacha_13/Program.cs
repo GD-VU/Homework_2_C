19360c4062d05ea6589bf638f4a86973c805f0db
Vasilij U <vasya-1001@yandex.ru>
2023-04-17 21:49:08 +0500

    Zadacha 13 particularly completed

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(System.Console.ReadLine());
+System.Console.WriteLine($"Число, стоящее на третьей строчке: {print(num)}");
+
+// проверка числа что оно больше трех цифр
+void check(int a)
+{
+    if ((a/100)>0) print(a);
+    System.Console.WriteLine("Третьей цифры нет!");
+}
+// программа вывода третьей цифры
+double print(double i)
+{
+    //int i = 123;
+    // double
+    char[] arr = i.ToString().ToCharArray();
+    double l = arr.Length;
+    double x1 = i/(Math.Pow(10, l-3));
+    double ThirdNumber = (x1%10);
+    //string result = ThirdNumber.ToString("#.##");
+    return Math.Round(ThirdNumber, 0);
+}
